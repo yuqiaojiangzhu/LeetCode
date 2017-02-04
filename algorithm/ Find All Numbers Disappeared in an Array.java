deb509ab6c23dc3c0f09827d2ae69e26b13efd5c
yuqiaojiangzhu <yuqiaojiangzhu@outlook.com>
2017-02-04 13:29:02 +0800

    Create  Find All Numbers Disappeared in an Array.java

---
+++ algorithm/ Find All Numbers Disappeared in an Array.java
@@ -0,0 +1,26 @@
+/**
+  *Given an array of integers where 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
+  *Find all the elements of [1, n] inclusive that do not appear in this array.
+  *Could you do it without extra space and in O(n) runtime? You may assume the returned list does not count as extra space.
+  */
+
+public class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> result=new ArrayList<Integer>();    //需要返回的List
+        if(nums==null || nums.length==0){
+            return result;
+        }
+        
+        int len=nums.length;    //数组长度
+        for(int i=0;i<len;i++){
+            nums[(nums[i]-1)%len]+=len;
+        }
+        
+        for(int i=0;i<len;i++){
+            if(nums[i]-1<len) {
+                result.add(i+1);
+            }  
+        }
+        return result;
+    }
+}
