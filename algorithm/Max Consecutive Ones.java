dfcc6e27ab73cd47297c9d9471bfb508fdf5cc18
yuqiaojiangzhu <yuqiaojiangzhu@outlook.com>
2017-02-04 12:08:18 +0800

    Update Max Consecutive Ones.java

--- algorithm/Max Consecutive Ones.java
+++ algorithm/Max Consecutive Ones.java
@@ -12,20 +12,16 @@ public class Solution {
             return 0;
         }
         
-        int num=nums[0];    //num是当前记录数
-        int len=1,beg=0;   //分别记录当前记录数的连续长度和初始位置
-        int maxLen=len,maxBeg=beg;     //分别记录最大连续数长度和初始位置
+        int len=0;   //分别记录当前记录数的连续长度
+        int maxLen=len;     //分别记录最大连续数长度
         
         for(int i=0;i<nums.length;i++){
-            if(nums[i]==num){
+            if(nums[i]==1){
                 len++;
                 if(len>maxLen){
                     maxLen=len;
-                    maxBeg=beg;
                 }
             }else{
-                num=nums[i];
-                beg=i;
                 len=0;
             }
         }
